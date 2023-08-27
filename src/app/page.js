54e8845440a8434d1326dd36b779295523b5adc4
ArturZimbarg <86981718+ArturZimbarg@users.noreply.github.com>
2023-08-27 17:04:34 -0300

    versão pré atualizações de acordo com a ultima aula do prof

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 import Image from 'next/image'
+import Titulo from './components/titulo'
 
 export default function Home() {
   return ( //JSX
     <>
-      <nav className="bg-slate-900 p-4">
-        <h1 className="text-3xl font-bold">Fiap Filmes</h1>
-      </nav>
+      <ul className="p-4 bg-yellow-400  ">
+        <li><a className="text-3xl font-bold ">Suas séries flix </a></li>
+        <li><a>em alta</a></li>
+        <li><a>lançamentos</a></li>
+        <li><a>comédia</a></li>
+        <li><a>aventura</a></li>
+        <li><a>suspense</a></li>
+        <li><a>drama</a></li>
+        
+      </ul>
 
     </>
   )
